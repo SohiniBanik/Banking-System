27cc57363d489bc95d6da2c3d849a07b7b1fb7bd
SohiniBanik <71387307+SohiniBanik@users.noreply.github.com>
2021-04-08 21:31:39 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,35 @@
+use bank
+db.users.insertOne({name:"Sohini Banik",money:12000,to:"Kamlash Singh",date:"March 6"})
+
+db.users.insertOne({name:"Sreshtha",money:32000,to:"Kamna ",date:"March 8"})
+db.users.insertOne({name:"Samrat",money:42000,to:"Vabna",date:"March 12"})
+db.users.insertOne({name:"Mohan",money:100000,to:"Madhavi ",date:"April 1"})
+db.users.insertOne({name:"Samira",money:42000,to:"Madhuban",date:"January 12"})
+db.users.insertOne({name:"Sumit",money:2000,to:"champa ",date:"March 21"})
+db.users.insertOne({name:"Simran",money:4200,to:"Vabna",date:"April 12"})
+db.users.insertOne({name:"Sumedh",money:2000,to:"Mallika ",date:"DEcember 21"})
+db.users.insertOne({name:"Sona",money:4200,to:"MOna",date:"April 30"})
+
+
+db.customers.insertOne({name:"Sohini Banik",balance:112000,email:"sohinibanik@gmail.com"})
+
+db.customers.insertOne({name:"Sreshtha",balance:52000,email:"shres@gmail.com"})
+
+db.customers.insertOne({name:"Kamna",balance:42000,email:"kamna@gmail.com"})
+
+db.customers.insertOne({name:"Mohan",balance:89000,email:"mohan@gmail.com"})
+
+db.customers.insertOne({name:"Samrat",balance:92000,email:"samrat@gmail.com"})
+
+db.customers.insertOne({name:"Kamlash Singh",balance:110000,email:"kamlashsingh@gmail.com"})
+
+db.customers.insertOne({name:"Sumit",balance:112000,email:"sumithsingh@gmail.com"})
+
+db.customers.insertOne({name:"Sumedh",balance:112000,email:"beatking@gmail.com"})
+
+db.customers.insertOne({name:"Mallika",balance:112000,email:"mallikasingh@gmail.com"})
+
+db.customers.insertOne({name:"Madhavi",balance:112000,email:"madhavi@gmail.com"})
+
+
+
